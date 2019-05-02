c815ef69b52f182ad60a03a58f7f8113b16b4b02
cqkd6381 <cqkd6381@163.com>
2019-05-03 01:38:44 +0800

    实现表单验证&&验证消息自定义

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,7 +10,15 @@ import router from './routes';
 
 import App from './components/App';
 
+import zh_CN from './locale/zh_CN';
+
+import VeeValidate, { Validator } from 'vee-validate';
+
 Vue.use(VueRouter);
+
+Vue.use(VeeValidate);
+
+Validator.localize('zh_CN', zh_CN);
 
 Vue.component('app', App);
 
--- resources/js/components/register/RegisterForm.vue
+++ resources/js/components/register/RegisterForm.vue
@@ -5,8 +5,12 @@
             <label for="name" class="col-md-4 col-form-label text-md-right">用户名</label>
 
             <div class="col-md-6">
-                <input v-model="name" id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
-
+                <input v-model="name"
+                       :class="{ 'is-invalid' : errors.has('name') }"
+                       v-validate="'required|min:4'"
+                       data-vv-as="用户名 "
+                       id="name" type="text" class="form-control" name="name" required autocomplete="name">
+                <span class="invalid-feedback" v-show="errors.has('name')">{{errors.first('name')}}</span>
             </div>
         </div>
 
@@ -14,7 +18,12 @@
             <label for="email" class="col-md-4 col-form-label text-md-right">邮箱</label>
 
             <div class="col-md-6">
-                <input v-model="email" id="email" type="email" class="form-control" name="email" required autocomplete="email">
+                <input v-model="email"
+                       :class="{ 'is-invalid' : errors.has('email') }"
+                       v-validate="{ rules: {required: true, email: true} }"
+                       data-vv-as="邮箱 "
+                       id="email" type="email" class="form-control" name="email" required>
+                <span class="invalid-feedback" v-show="errors.has('email')">{{errors.first('email')}}</span>
             </div>
         </div>
 
@@ -22,7 +31,12 @@
             <label for="password" class="col-md-4 col-form-label text-md-right">密码</label>
 
             <div class="col-md-6">
-                <input v-model="password" id="password" type="password" class="form-control" name="password" required autocomplete="new-password">
+                <input v-model="password"
+                       :class="{ 'is-invalid' : errors.has('password') }"
+                       v-validate="{ rules: {required: true, min: 8} }"
+                       data-vv-as="密码 "
+                       id="password" type="password" class="form-control" name="password" required>
+                <span class="invalid-feedback" v-show="errors.has('password')">{{errors.first('password')}}</span>
             </div>
         </div>
 
@@ -30,7 +44,11 @@
             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">确认密码</label>
 
             <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                <input :class="{ 'is-invalid' : errors.has('password_confirmation') }"
+                       v-validate="{ rules: {required: true, min: 8, confirmed: 'password'} }"
+                       data-vv-as="确认密码 "
+                       id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                <span class="invalid-feedback" v-show="errors.has('password_confirmation')">{{errors.first('password_confirmation')}}</span>
             </div>
         </div>
 
